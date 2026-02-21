4a056b3acc404ad3e3796250498ffaf53eb9b024
Christian Walker <christiandwalker2021@gmail.com>
2026-02-21 13:03:25 -0700

    Removes unneeded functions. Can be added back later, but dead code for now

--- apps/api/src/lib/functions/database.ts
+++ apps/api/src/lib/functions/database.ts
@@ -4,40 +4,6 @@ import type { LoggingOptions, LoggingType } from "../types";
 import { type Context } from "hono";
 import { isInDevMode } from ".";
 
-/**
- * Fetches a database dump from a Turso database instance.
- * @param databseName The name of the database.
- * @param organizationSlug The organization slug associated with the database.
- */
-export async function getDatabaseDumpTurso(
-	databseName: string,
-	organizationSlug: string,
-) {
-	const res = await fetch(
-		`https://${databseName}-${organizationSlug}.turso.io/dump`,
-		{
-			method: "GET",
-			headers: new Headers({
-				// Authorization: `Bearer ${env.BACKUPS_DB_BEARER}`,
-			}),
-		},
-	);
-
-	if (!res.ok) {
-		throw new Error(
-			`Failed to get database dump: ${res.status} ${res.statusText}`,
-		);
-	}
-
-	return res.text();
-}
-
-/**
- * Checks the database connection by pinging it and querying for it's table count.
- * Function will take in an database information and the type to make the appropriate query.
- */
-export async function pingDatabase() {}
-
 export function isSiteAdminUser(
 	permissionEnum: NonNullable<UserType>["siteRole"],
 ): boolean {
